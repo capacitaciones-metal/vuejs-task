452a2de2aa1295b03715817d6b0594402eb3b54c
cristian.incarnato@sondeos.com.ar <Neverwinter2.!>
2019-06-27 18:01:43 -0300

    Se agrega vuetify

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store/Store'
+import Vuetify from 'vuetify'
+import './plugins/vuetify'
 
 Vue.config.productionTip = false
+
+
+Vue.use(Vuetify)
 
 var eventHub = new Vue()
 Vue.prototype.$bus = eventHub
